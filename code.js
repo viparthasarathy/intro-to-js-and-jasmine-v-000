2815753a0d4c33f5ecd3b5a41f830893da87b603
viparthasarathy <viparthasarathy@gmail.com>
2016-04-13 10:37:04 -0700

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(flavor){
+  return "I love " + flavor;
+}
+
+function shouting(phrase){
+  return phrase.toUpperCase();
+}
+
+function roundDown(value){
+  return Math.floor(value);
+}
+
+function theTruth(){
+  return true;
+}
